476ad31055d626034c5d67e3a492d91654955341
unknown <varadesharad@gmail.com>
2020-04-19 19:32:53 +0530

    Adding: create an array and store value in array

--- AdditionOfTwoNumber.sh
+++ AdditionOfTwoNumber.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+declare -a Arithmetic
 read -p "Enter the A value: " a
 read -p "Enter the B value: " b
 
@@ -9,4 +10,9 @@ k=$(( $a / $b ))
 p=$(( $a * $b ))
 
 c=$(( $a + $b ))
-echo $c
+
+counter=0
+Arithmetic[$counter]=$k;
+Arithmetic[$((++counter))]=$p;
+Arithmetic[$((++counter))]=$c;
+echo  ${Arithmetic[@]}
